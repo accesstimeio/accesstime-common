c01709de2e6c9ba10b001c42d549a6247649ce24
mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>
2025-01-01 18:43:46 +0300

    chore: update AccessVote address

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -6,7 +6,7 @@ import { AccessVoteABI } from "./abis/AccessVote";
 
 export class Contract {
     public static factories: Address[] = [zeroAddress, "0x84Ec87B41272223755AdD5f2A8271290aD5d87f0"];  // base, baseSepolia
-    public static votes: Address[] = [zeroAddress, "0xc81087D2D32223aCBfA5143Af996C29b8C7628f1"];  // base, baseSepolia
+    public static votes: Address[] = [zeroAddress, "0x13c3D0435fB0bE55dCAb45EB32393F64695497Cc"];  // base, baseSepolia
     public static abis = {
         factory: AccessTimeFactoryABI,
         accessTime: AccessTimeABI,
